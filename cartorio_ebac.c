6fa818e3c8c4e2431a0854650cc4cd931352526a
Ayato <dantebarkindakwt2@gmail.com>
2024-10-22 14:40:28 -0300

    Opção de saída
    Inclusão da opção de saída do sistema.

--- cartorio_ebac.c
+++ cartorio_ebac.c
@@ -123,6 +123,7 @@ int main()
 	    printf("\t1 - Registrar usuários\n");
 	    printf("\t2 - Consultar usuários\n");
 	    printf("\t3 - Deletar usuários\n\n");
+	    printf("\t4 - Sair\n\n");
 	    printf("Opção: "); // fim do menu
 	
 	    scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -142,6 +143,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("O sistema está sendo fechado!");
+			return 0;
 			break;
 			
 			default:
